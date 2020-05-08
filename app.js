ec7644f79d4a2d9894b5af473669f642947479fc
Yugant Kachroo <yugant35@gmail.com>
2020-05-08 20:14:58 +0530

    callback abstraction

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url ='http://api.weatherstack.com/current?access_key=a21f077d1e34429e8b99745463c2202c&query=New%20York?'
 
-request({url: url, json:true}, (error,response)=>{
-    if(error)
-    {
-        console.log('no network connection')
-    }
-    else if(response.body.error)
-    {
+forecast(44.1545, -75.7088, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
 
-        console.log('unable to find location')
 
-    }
-    else
-    {
-        console.log(response.body.current.weather_descriptions + ' It is currently ' + response.body.current.temperature)
-    
-    }
-    
+geocode('new York', (error,data)=>{
+  console.log('Error', error)
+  console.log('Data', data)
 
-})
-
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieXVnYW50IiwiYSI6ImNrOXZlcWk3ZjBheGwzbW1vZTk3Z25kZGUifQ.YhSqsZWvGQ9vmyLDScaymQ&limit=1'
-request({url: geocodeURL, json:true},(error,response)=>
-{
-    if(error)
-    {
-        console.log('no network connection')
-    }
-    else if(response.body.features.length === 0)
-    {
-        console.log('Unable to find location. Try another search')
-
-    }
-    else{
-        const lat = response.body.features[0].center[1]
-        const long = response.body.features[0].center[0]
-        console.log(lat, long)
-    
-    }
-    
 })
