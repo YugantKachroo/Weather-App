513a5e818bb6feaf9b0f83464cafb9a1a68df0a8
Yugant Kachroo <yugant35@gmail.com>
2020-05-31 01:33:42 +0530

    query response added

--- Web-Server/public/js/app.js
+++ Web-Server/public/js/app.js
@@ -2,18 +2,25 @@ console.log('Client side javascript file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const loc = search.value
-    fetch('http://localhost:3000/weather?address=' + loc).then((response)=>
+    messageOne.textContent = ''
+    messageOne.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + loc ).then((response)=>
 {
     response.json().then((data)=>{
-        if(data.error)
-        console.log(data.error)
+        if(data.error){
+            messageOne.textContent = data.error
+        
+        }
         else{
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
 
     })
